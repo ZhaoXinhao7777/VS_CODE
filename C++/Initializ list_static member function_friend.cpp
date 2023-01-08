8b644477795fc91b88185e7b1cf6c3162de92473
ZhaoXinhao7777 <1692532445@qq.com>
2023-01-08 18:10:26 +0800

    初始化列表&静态成员函数&友元

--- C++/Initializ list_static member function_friend.cpp
+++ C++/Initializ list_static member function_friend.cpp
@@ -1798,3 +1798,335 @@ int main()
     return 0;
 }
 #endif
+
+// 完整的Date类
+#include <iostream>
+using namespace std;
+
+class Date
+{
+private:
+    int _year;
+    int _month;
+    int _day;
+
+private:
+    int GetDaysOfMonth(int year, int month) const
+    {
+        int days[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if (2 == month && IsLeapYear(year))
+        {
+            days[2] += 1;
+        }
+        return days[month];
+    }
+
+public:
+    Date(size_t year = 1900, size_t month = 1, size_t day = 1)
+        : _year(year), _month(month), _day(day)
+    {
+        // 一定要检测日期的合法性
+        if (!((year > 0) &&
+              (month >= 1 && month <= 12) &&
+              (day >= 1 && day <= GetDaysOfMonth(year, month))))
+        {
+            cout << "Parameter invalid,Set the date class to the default date:1900 01 01" << endl;
+            _year = 1900;
+            _month = 1;
+            _day = 1;
+        }
+    }
+    Date(const Date &d)
+        : _year(d._year), _month(d._month), _day(d._day)
+    {
+    }
+
+    // 重载赋值运算符
+    Date &operator=(const Date &d)
+    {
+        if (this != &d)
+        {
+            _year = d._year;
+            _month = d._month;
+            _day = d._day;
+        }
+        return *this;
+    }
+
+    // a+b
+    // d1+100 求100天后是哪一天
+    Date operator+(int days) const
+    {
+        if (days < 0)
+        {
+            // 加上一个负数等于减一个正数
+            return *this - (0 - days);
+        }
+
+        // 注意： + 不需要修改左右操作数中的内容，即days和this都不能修改
+        // 因此，加完之后结果只能通过临时变量保存起来
+
+        Date temp(*this);
+        temp._day += days;
+
+        // 注意，加完之后，一定要检测temp的日期合法性
+        // 将日期调成合法的结果
+        int daysOfMonth = 0;
+        while (temp._day > (daysOfMonth = temp.GetDaysOfMonth(temp._year, temp._month)))
+        {
+            temp._day -= daysOfMonth;
+            temp._month += 1;
+            if (temp._month == 13)
+            {
+                temp._year += 1;
+                temp._month = 1;
+            }
+        }
+        return temp;
+    }
+
+    // d1-100天后是哪一天
+    Date operator-(int days) const
+    {
+        if (days < 0)
+        {
+            // 减去一个负数等于加上一个正数
+            return *this + (0 - days);
+        }
+
+        // 注意：-不需要修改左右操作数的内容，即days和this都不能被修改
+        //  因此加完之后只能通过临时对象保存起来
+        Date temp(*this);
+        temp._day -= days;
+        while (temp._day <= 0)
+        {
+            // 对非法的数字进行调整，加前一个月的天数
+            temp._month -= 1;
+            if (0 == temp._month)
+            {
+                temp._year -= 1;
+                temp._month = 12;
+            }
+            temp._day += temp.GetDaysOfMonth(temp._year, temp._month);
+        }
+        return temp;
+    }
+    // d1-d2 还剩多少天
+    int operator-(const Date &d) const
+    {
+        Date minDate(*this);
+        Date maxDate(d);
+        if (minDate > maxDate)
+        {
+            minDate = d;
+            maxDate = *this;
+        }
+        int count = 0;
+        // minDate<=maxDate;
+        while (minDate != maxDate)
+        {
+            count++;
+            ++minDate;
+        }
+        return count;
+    }
+    Date &operator++()
+    {
+        *this = *this + 1;
+        return *this;
+    }
+    Date operator++(int)
+    {
+        Date temp(*this);
+        *this = *this + 1;
+        return temp;
+    }
+    Date &operator--()
+    {
+        *this = *this - 1;
+        return *this;
+    }
+    Date operator--(int)
+    {
+        Date temp(*this);
+        *this = *this - 1;
+        return temp;
+    }
+    bool operator!=(const Date &d) const
+    {
+        return !(*this == d);
+    }
+    bool operator==(const Date &d) const
+    {
+        return _year == d._year &&
+               _month == d._month &&
+               _day == d._day;
+    }
+    bool operator>(const Date &d) const
+    {
+        if ((_year > d._year) ||
+            (_year == d._year && _month > d._month) ||
+            (_year == d._year && _month == d._month && _day > d._day))
+            return true;
+        else
+            return false;
+    }
+    bool operator<(const Date &d) const
+    {
+        return !(*this > d || *this == d);
+    }
+    bool operator>=(const Date &d) const
+    {
+        return *this > d || *this == d;
+    }
+    bool operator<=(const Date &d) const
+    {
+        return *this < d || *this == d;
+    }
+    ~Date()
+    {
+        // 一个疑问？为啥在执行TestDate5()时候，出现执行多条析构函数？什么情况下导致多次创建对象？--离谱。...待验证
+        //  cout << "~Date()" << endl;
+    }
+
+public:
+    void SetYear(int year)
+    {
+        _year = year;
+    }
+    int GetYear() const
+    {
+        return _year;
+    }
+
+    void SetMonth(int month)
+    {
+        _month = month;
+    }
+    int GetMonth() const
+    {
+        return _month;
+    }
+
+    void SetDay(int day)
+    {
+        _day = day;
+    }
+    int GetDay() const
+    {
+        return _day;
+    }
+
+    friend ostream &operator<<(ostream &cout, const Date &d);
+    friend istream &operator>>(istream &cin, Date &d);
+
+    // 检测一个年份是否为闰年，与当前对象没有关系
+    static bool IsLeapYear(int year)
+    {
+        if ((0 == year % 4 && 0 != year % 100) || (0 == year % 400))
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    // 检测当前对象是否为闰年
+    bool IsLeapYear() const
+    {
+        return IsLeapYear(_year);
+    }
+};
+
+ostream &operator<<(ostream &cout, const Date &d)
+{
+    cout << d._year << " " << d._month << " " << d._day;
+    return cout;
+}
+istream &operator>>(istream &cin, Date &d)
+{
+    cin >> d._year >> d._month >> d._day;
+    return cin;
+}
+
+void TestDate1()
+{
+    Date d1(2022, 11, 22);
+    cout << d1 << endl;
+
+    Date d2(2022, 11, 100);
+    cout << d2 << endl;
+
+    // 检测d2对象的日期是否为闰年
+    if (d2.IsLeapYear())
+    {
+        cout << d2.GetYear() << "is leap year!" << endl;
+    }
+    else
+        cout << d2.GetYear() << "is not leap year!" << endl;
+
+    // 想知道2000年是否为闰年
+    if (Date::IsLeapYear(2000))
+    {
+        cout << "2000 is leap year" << endl;
+    }
+    else
+        cout << "2000 is not leap year" << endl;
+}
+
+void TestDate2()
+{
+    Date d1(2022, 11, 22);
+    cout << d1 << endl;
+
+    // d1 = d1 + 5;
+    // cout << d1.GetDay() << endl;
+    d1 = d1 + 100;
+    cout << d1 << endl;
+}
+void TestDate3()
+{
+    Date d1(2022, 11, 22);
+    cout << d1 << endl;
+    d1 = d1 - (-100);
+    cout << d1 << endl;
+}
+
+void TestDate4()
+{
+    Date d1(2022, 11, 22);
+    cout << d1 << endl;
+
+    Date d2;
+    d2 = d1++;
+    cout << d1 << " " << d2 << endl;
+
+    d2 = ++d1;
+    cout << d1 << " " << d2 << endl;
+
+    d2 = d1--;
+    cout << d1 << " " << d2 << endl;
+
+    d2-- = d1;
+    cout << d1 << " " << d2 << endl;
+}
+void TestDate5()
+{
+    Date d1(2022, 11, 22);
+    cout << d1 << endl;
+
+    // Date d2(2023, 1, 1);
+    Date d2(2022, 5, 1);
+    cout << d2 - d1 << endl;
+}
+int main()
+{
+    // TestDate1();
+    // TestDate2();
+    // TestDate3();
+    // TestDate4();
+    TestDate5();
+    system("pause");
+    return 0;
+}
